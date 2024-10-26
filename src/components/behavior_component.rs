f5c16385635194d3236ef580d103dcedca2e295f
Washington Irving <iamwashingyou@gmail.com>
2024-10-27 01:42:39 +0300

    add behavior component

---
+++ src/components/behavior_component.rs
@@ -0,0 +1,17 @@
+pub trait BehaviorComponent {
+    fn on_start(&mut self);
+    fn on_update(&mut self);
+    fn on_destroy(&mut self);
+
+    fn start(&mut self) {
+        self.on_start();
+    }
+
+    fn update(&mut self) {
+        self.on_update();
+    }
+
+    fn destroy(&mut self) {
+        self.on_destroy();
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,9 @@
+mod behavior_component;
 mod sprite_component;
 mod tag_component;
 mod transform_component;
 
+pub use behavior_component::BehaviorComponent;
 pub use sprite_component::SpriteComponent;
 pub use tag_component::TagComponent;
 pub use transform_component::TransformComponent;
