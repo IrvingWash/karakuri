45f599f50e0246323c87dec20d40b0946c9e524e
Washington Irving <iamwashingyou@gmail.com>
2024-10-27 01:39:30 +0300

    add tag component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,7 @@
 mod sprite_component;
+mod tag_component;
 mod transform_component;
 
 pub use sprite_component::SpriteComponent;
+pub use tag_component::TagComponent;
 pub use transform_component::TransformComponent;
---
+++ src/components/tag_component.rs
@@ -0,0 +1,13 @@
+pub struct TagComponent {
+    value: String,
+}
+
+impl TagComponent {
+    pub fn new(value: String) -> Self {
+        Self { value }
+    }
+
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+}
