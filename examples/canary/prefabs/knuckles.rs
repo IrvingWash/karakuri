785a7547f17c6741bbfbb949f7e504a2dc7d45c1
Washington Irving <iamwashingyou@gmail.com>
2024-10-27 12:50:05 +0300

    refactor knuckles on update

--- examples/canary/prefabs/knuckles.rs
+++ examples/canary/prefabs/knuckles.rs
@@ -1,6 +1,7 @@
 use karakuri::components::{
     BehaviorComponent, ComponentPayload, Ctx, SpriteComponent, TagComponent, TransformComponent,
 };
+use kec::Entity;
 use kmath::Vector2;
 use kutils::{Color, Size};
 
@@ -22,23 +23,18 @@ impl BehaviorComponent for Knuckles {
     fn on_start(&mut self) {}
 
     fn on_update(&mut self, ctx: Ctx) {
-        let mut transform_components = ctx.registry.get_component_vec_mut::<TransformComponent>();
+        let tails_transform = ctx
+            .registry
+            .get_component::<TransformComponent>(&Entity::new(1))
+            .unwrap();
+        let mut knuckles_transfrom = ctx
+            .registry
+            .get_component_mut::<TransformComponent>(ctx.entity)
+            .unwrap();
 
-        // TODO: This is a hack!
-        let tails = 1;
-        let tails_position = {
-            transform_components[tails]
-                .as_ref()
-                .unwrap()
-                .position
-                .create_copy()
-        };
-
-        let knuckles_transform = transform_components[ctx.entity.id()].as_mut().unwrap();
-
-        knuckles_transform
+        knuckles_transfrom
             .position
-            .move_towards(&tails_position, self.speed * ctx.delta_time);
+            .move_towards(&tails_transform.position, self.speed * ctx.delta_time);
     }
 
     fn on_destroy(&mut self) {}
