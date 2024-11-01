8a3a3f54d60484b0f02c10f980e873f0fd1b935d
Washington Irving <iamwashingyou@gmail.com>
2024-11-01 23:00:33 +0300

    refactor query

--- crates/kec/src/query.rs
+++ crates/kec/src/query.rs
@@ -4,21 +4,21 @@ use crate::{errors::panic_registered_without_id, Entity, Registry, Signature};
 
 pub struct Query<'a> {
     registry: &'a mut Registry,
-    component_ids: Vec<usize>,
+    queried_component_ids: Vec<usize>,
 }
 
 impl<'a> Query<'a> {
     pub fn new(registry: &'a mut Registry) -> Self {
         Self {
             registry,
-            component_ids: Vec::with_capacity(64),
+            queried_component_ids: Vec::with_capacity(64),
         }
     }
 
     pub fn build(mut self) -> Vec<Entity> {
-        self.component_ids.dedup();
+        self.queried_component_ids.dedup();
 
-        let signature = Signature::from(self.component_ids);
+        let signature = Signature::from(self.queried_component_ids);
 
         if signature.is_empty() {
             return Vec::new();
@@ -42,11 +42,12 @@ impl<'a> Query<'a> {
 
         match self.registry.component_ids().get(&component_type) {
             Some(component_id) => {
-                self.component_ids.push(*component_id);
+                self.queried_component_ids.push(*component_id);
             }
             None => {
                 self.registry.register_component::<T>();
-                self.component_ids.push(
+
+                self.queried_component_ids.push(
                     *self
                         .registry
                         .component_ids()
