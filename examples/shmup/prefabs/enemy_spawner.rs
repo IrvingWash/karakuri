59cafa75b1628a48912bca666b9431a4ee4ec5e7
Washington Irving <iamwashingyou@gmail.com>
2024-11-04 13:43:02 +0300

    add enemy spawner to shmup

---
+++ examples/shmup/prefabs/enemy_spawner.rs
@@ -0,0 +1,44 @@
+use karakuri::components::{BehaviorComponent, ComponentPayload, TagComponent};
+use kmath::Vector2;
+use rand::Rng;
+
+use super::enemy_prefab;
+
+pub fn enemy_spawner_prefab() -> ComponentPayload {
+    ComponentPayload {
+        tag: Some(TagComponent::new(String::from("enemy-spawner"))),
+        behavior: Some(Box::new(EnemySpawner::new(1000.0))),
+        ..Default::default()
+    }
+}
+
+#[derive(Debug)]
+struct EnemySpawner {
+    rate: f64,
+    timer_id: i64,
+}
+
+impl EnemySpawner {
+    fn new(rate: f64) -> Self {
+        Self { rate, timer_id: -1 }
+    }
+}
+
+impl BehaviorComponent for EnemySpawner {
+    fn on_start(&mut self, ctx: karakuri::components::Ctx) {
+        self.timer_id = ctx.timer.set_interval(self.rate) as i64;
+    }
+
+    fn on_update(&mut self, ctx: karakuri::components::Ctx) {
+        if ctx.timer.is_done(self.timer_id as usize) {
+            ctx.spawner.add_entity(enemy_prefab(Vector2::new(
+                800.0 * rand::thread_rng().gen::<f64>(),
+                100.0,
+            )));
+        }
+    }
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+}
--- examples/shmup/prefabs/mod.rs
+++ examples/shmup/prefabs/mod.rs
@@ -1,8 +1,10 @@
 mod enemy;
+mod enemy_spawner;
 mod laser_destroyer;
 mod player;
 mod player_laser;
 
 pub use enemy::enemy_prefab;
+pub use enemy_spawner::enemy_spawner_prefab;
 pub use laser_destroyer::laser_destroyer_prefab;
 pub use player::player_prefab;
