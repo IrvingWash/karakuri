7e0ba86f099a6c33e44cdcbf83728eb9fcc948eb
Washington Irving <iamwashingyou@gmail.com>
2024-11-04 12:41:58 +0300

    start adding tests

--- crates/kwindow/src/timer.rs
+++ crates/kwindow/src/timer.rs
@@ -1,51 +1,83 @@
 use std::collections::HashMap;
 
 #[derive(Debug)]
-struct Interval {
+struct Timeout {
     duration: f64,
     start_time: f64,
+    callback: fn(),
 }
 
 #[derive(Debug, Default)]
 pub struct Timer {
     next_id: usize,
-    intervals: HashMap<usize, Interval>,
+    timeouts: HashMap<usize, Timeout>,
 }
 
 impl Timer {
     pub fn new() -> Self {
         Self {
             next_id: 0,
-            intervals: HashMap::with_capacity(64),
+            timeouts: HashMap::with_capacity(64),
         }
     }
 
-    pub fn set_interval(&mut self, duration: f64) -> usize {
+    pub fn set_timeout(&mut self, callback: fn(), duration: f64) -> usize {
         let id = self.next_id;
         self.next_id += 1;
 
-        self.intervals.insert(
+        self.timeouts.insert(
             id,
-            Interval {
+            Timeout {
                 duration,
                 start_time: 0.0,
+                callback,
             },
         );
 
         id
     }
 
-    pub fn update(&mut self, time: f64) {
-        let mut intervals_to_remove: Vec<usize> = Vec::with_capacity(self.intervals.capacity());
+    pub fn clear_timeout(&mut self, id: usize) {
+        self.timeouts.remove(&id);
+    }
 
-        for (id, interval) in &self.intervals {
-            if interval.duration + interval.start_time >= time {
-                intervals_to_remove.push(*id);
+    pub fn update(&mut self, time: f64) {
+        let mut timeouts_to_remove: Vec<usize> = Vec::with_capacity(self.timeouts.capacity());
+
+        for (id, timeout) in &self.timeouts {
+            if timeout.duration + timeout.start_time >= time {
+                (timeout.callback)();
+
+                timeouts_to_remove.push(*id);
             }
         }
 
-        for id in intervals_to_remove {
-            self.intervals.remove(&id);
+        for id in timeouts_to_remove {
+            self.timeouts.remove(&id);
         }
+    }
+}
+
+#[cfg(test)]
+mod timer_tests {
+    use super::Timer;
+
+    struct Player {
+        health: u8,
+    }
+
+    impl Player {
+        fn heal(&mut self) {
+            self.health += 1;
+        }
+    }
+
+    #[test]
+    fn test_timeouts() {
+        let mut timer = Timer::new();
+
+        let mut player = Player { health: 10 };
+
+        timer.set_timeout(Player::heal, 1000.0);
     }
 }
