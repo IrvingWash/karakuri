acc61ce0e38d06a728e2b8a07ac40ff1ae32f739
Washington Irving <iamwashingyou@gmail.com>
2024-11-05 22:27:41 +0300

    refactor logger

--- crates/klogger/src/lib.rs
+++ crates/klogger/src/lib.rs
@@ -1,3 +1,5 @@
+static mut LOGS: Vec<String> = Vec::new();
+
 const KARAKURI: &str = "karakuri";
 const ANSI_START: &str = "\x1b[0;";
 const ANSI_END: &str = "\x1b[0m";
@@ -6,17 +8,27 @@ const RED: &str = "31m";
 const CYAN: &str = "35m";
 
 pub fn info(message: &str) {
-    println!("{KARAKURI} info: {ANSI_START}{BLUE}{message}{ANSI_END}");
+    print(message, BLUE)
 }
 
 pub fn warn(message: &str) {
-    println!("{KARAKURI} warn: {ANSI_START}{CYAN}{message}{ANSI_END}");
+    print(message, CYAN)
 }
 
 pub fn error(message: &str) {
-    println!("{KARAKURI} error: {ANSI_START}{RED}{message}{ANSI_END}");
+    print(message, RED);
 }
 
 pub fn terminate(message: &str) -> ! {
     panic!("{KARAKURI} terminate: {ANSI_START}{RED}{message}{ANSI_END}");
+}
+
+fn print(message: &str, color: &'static str) {
+    let value = format!("{KARAKURI} error: {ANSI_START}{color}{message}{ANSI_END}");
+
+    println!("{value}");
+
+    unsafe {
+        LOGS.push(value);
+    }
 }
