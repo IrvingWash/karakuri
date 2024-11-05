ababcb53b3e87b07b71ffa7f6d175b57859eba13
Washington Irving <iamwashingyou@gmail.com>
2024-11-05 22:02:47 +0300

    add logger

---
+++ crates/klogger/src/lib.rs
@@ -0,0 +1,18 @@
+const KARAKURI: &str = "karakuri";
+const ANSI_START: &str = "\x1b[0;";
+const ANSI_END: &str = "\x1b[0m";
+const BLUE: &str = "34m";
+const RED: &str = "31m";
+const CYAN: &str = "35m";
+
+pub fn info(message: &str) {
+    println!("{KARAKURI}: {ANSI_START}{BLUE}{message}{ANSI_END}");
+}
+
+pub fn warn(message: &str) {
+    println!("{KARAKURI}: {ANSI_START}{CYAN}{message}{ANSI_END}");
+}
+
+pub fn error(message: &str) {
+    println!("{KARAKURI}: {ANSI_START}{RED}{message}{ANSI_END}");
+}
