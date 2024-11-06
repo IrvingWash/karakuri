421f769e629b6f91a3ebe80a4dea7871dcb8cb1e
Washington Irving <iamwashingyou@gmail.com>
2024-11-06 22:11:26 +0300

    refactor kec a bit

--- crates/kec/src/entity.rs
+++ crates/kec/src/entity.rs
@@ -2,17 +2,17 @@ use std::{cmp::PartialEq, hash::Hash};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Entity {
-    id: usize,
+    key: usize,
     unique_id: usize,
 }
 
 impl Entity {
-    pub fn new(id: usize, unique_id: usize) -> Self {
-        Self { id, unique_id }
+    pub fn new(key: usize, unique_id: usize) -> Self {
+        Self { key, unique_id }
     }
 
-    pub fn id(&self) -> usize {
-        self.id
+    pub fn key(&self) -> usize {
+        self.key
     }
 
     pub fn unique_id(&self) -> usize {
